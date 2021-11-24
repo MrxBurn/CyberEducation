d64569a39a2a505c7651afd740a08ecd798297af
MrxBurn <xburngaming@gmail.com>
2021-11-24 11:53:53 +0000

    Homepage work in progress

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -5,6 +5,7 @@ import 'package:cyber_education/main.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:webviewx/webviewx.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 import 'quiz.dart';
 import 'retest.dart';
@@ -25,7 +26,7 @@ class _HomepageState extends State<Homepage> {
   //Variables
 
   var firstName = '';
-  final homeRoute = '/Homepage()';
+
   bool isNewRouteSameAsCurrent = false;
 
   final bool _customTileExpanded = false;
@@ -59,15 +60,14 @@ class _HomepageState extends State<Homepage> {
                 leading: Icon(Icons.home_filled),
                 title: const Text('Home'),
                 onTap: () {
-                  Navigator.popUntil(context, (route) {
-                    if (route.settings.name == homeRoute) {
-                      isNewRouteSameAsCurrent = true;
-                    }
-                    return true;
-                  });
+                  var route = ModalRoute.of(context);
+                  var name = route?.settings.name;
 
-                  if (!isNewRouteSameAsCurrent) {
-                    Navigator.pushNamed(context, homeRoute);
+                  Navigator.pop(context);
+
+                  if (name != null && name != '/') {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => Homepage()));
                   }
                 },
               ),
@@ -103,87 +103,128 @@ class _HomepageState extends State<Homepage> {
             ],
           ),
         ),
-        body: ScrollConfiguration(
-            behavior:
-                ScrollConfiguration.of(context).copyWith(scrollbars: false),
+        body: Center(
             child: SingleChildScrollView(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  FutureBuilder<DocumentSnapshot>(
-                    future: userName.get(),
-                    builder: (BuildContext context,
-                        AsyncSnapshot<DocumentSnapshot> snapshot) {
-                      if (snapshot.hasError) {
-                        return const Text("Something went wrong");
-                      }
+          child: Column(
+            children: [
+              FutureBuilder<DocumentSnapshot>(
+                future: userName.get(),
+                builder: (BuildContext context,
+                    AsyncSnapshot<DocumentSnapshot> snapshot) {
+                  if (snapshot.hasError) {
+                    return const Text("Something went wrong");
+                  }
 
-                      if (snapshot.hasData && !snapshot.data!.exists) {
-                        return const Text("Document does not exist");
-                      }
+                  if (snapshot.hasData && !snapshot.data!.exists) {
+                    return const Text("Document does not exist");
+                  }
 
-                      if (snapshot.connectionState == ConnectionState.done) {
-                        Map<String, dynamic> data =
-                            snapshot.data!.data() as Map<String, dynamic>;
+                  if (snapshot.connectionState == ConnectionState.done) {
+                    Map<String, dynamic> data =
+                        snapshot.data!.data() as Map<String, dynamic>;
 
-                        return Padding(
-                            padding: EdgeInsets.only(
-                              top: 20,
-                              left: 10,
-                            ),
-                            child: Container(
-                                height: 150,
-                                width: 200,
-                                decoration: BoxDecoration(
-                                    color: Colors.blueGrey,
-                                    borderRadius:
-                                        BorderRadius.all(Radius.circular(15))),
-                                child: Column(
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.start,
-                                    children: [
-                                      Padding(
-                                          padding: const EdgeInsets.only(
-                                            top: 20,
-                                            left: 10,
-                                          ),
-                                          child: Text(
-                                            "Welcome ${data['firstName']}",
-                                            style: const TextStyle(
-                                              fontSize: 20,
-                                            ),
-                                          )),
-                                      Padding(
-                                          padding: EdgeInsets.only(
-                                              top: 10, left: 10),
-                                          child: Text(
-                                            "Score: ${data['score']}",
-                                            style: const TextStyle(
-                                              fontSize: 18,
-                                            ),
-                                          ))
-                                    ])));
-                      }
-                      return const Text("loading");
-                    },
-                  ),
-                  Padding(
-                    padding: EdgeInsets.only(top: 20, left: 10),
+                    return Padding(
+                        padding: EdgeInsets.only(
+                          top: 20,
+                          left: 10,
+                        ),
+                        child: Container(
+                            height: 150,
+                            width: 200,
+                            decoration: BoxDecoration(
+                                color: Colors.blueGrey,
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(15))),
+                            child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Padding(
+                                      padding: const EdgeInsets.only(
+                                        top: 20,
+                                        left: 10,
+                                      ),
+                                      child: Text(
+                                        "Welcome ${data['firstName']}",
+                                        style: const TextStyle(
+                                          fontSize: 20,
+                                        ),
+                                      )),
+                                  Padding(
+                                      padding:
+                                          EdgeInsets.only(top: 10, left: 10),
+                                      child: Text(
+                                        "Score: ${data['score']}",
+                                        style: const TextStyle(
+                                          fontSize: 18,
+                                        ),
+                                      ))
+                                ])));
+                  }
+                  return const Text("loading");
+                },
+              ),
+              //Useful websites part
+              Column(children: [
+                SizedBox(
+                    width: 600,
+                    child: Padding(
+                      padding: EdgeInsets.only(
+                        top: 20,
+                        left: 30,
+                      ),
+                      child: Text(
+                        'Useful websites:',
+                        style: TextStyle(
+                          fontSize: 20,
+                        ),
+                      ),
+                    )),
+                SizedBox(
+                    width: 600,
+                    child: Padding(
+                      padding: EdgeInsets.only(
+                        top: 10,
+                        left: 30,
+                      ),
+                      child: InkWell(
+                        child: Text(
+                          "National Cyber Security Centre's website",
+                          style: TextStyle(
+                            color: Colors.orange,
+                          ),
+                        ),
+                        onTap: () => launch(
+                          'https://www.ncsc.gov.uk/section/advice-guidance/all-topics',
+                        ),
+                      ),
+                    ))
+              ]),
+
+              //Most used cyber attacks section
+              SizedBox(
+                  width: 600,
+                  child: Padding(
+                    padding: EdgeInsets.only(top: 20, left: 30),
                     child: Text(
                       'The most used cyber attacks:',
                       style: TextStyle(
                         fontSize: 20,
                       ),
                     ),
-                  ),
-                  SizedBox(
-                      width: 600,
-                      child: Padding(
-                          padding: EdgeInsets.only(
-                            top: 20,
-                            left: 20,
-                          ),
+                  )),
+
+              //1st Expansion Tile
+              SizedBox(
+                  width: 600,
+                  child: Padding(
+                      padding: EdgeInsets.only(
+                        top: 20,
+                        left: 20,
+                      ),
+                      child: ClipRRect(
+                          borderRadius: BorderRadius.all(Radius.circular(15)),
                           child: ExpansionTile(
+                              collapsedBackgroundColor: Color(0xFF304e60),
                               title: const Text('Malware Attacks'),
                               leading: Image.asset(
                                 'images/malware.png',
@@ -216,54 +257,56 @@ class _HomepageState extends State<Homepage> {
                                         " Additionally, newer methods of ransomware infection have been observed. For example, vulnerable Web servers have been exploited as an entry point to gain access to an organization’s network.")
                                   ],
                                 )
-                              ]))),
-                  SizedBox(
-                    width: 600,
-                    child: Padding(
-                        padding: EdgeInsets.only(
-                          top: 20,
-                          left: 20,
+                              ])))),
+
+              //2nd Expansion Tile
+              SizedBox(
+                width: 600,
+                child: Padding(
+                    padding: EdgeInsets.only(
+                      top: 20,
+                      left: 20,
+                    ),
+                    child: ExpansionTile(
+                        title: const Text('Phishing'),
+                        leading: Image.asset(
+                          'images/phishing.png',
                         ),
-                        child: ExpansionTile(
-                            title: const Text('Phishing'),
-                            leading: Image.asset(
-                              'images/phishing.png',
-                            ),
-                            subtitle: const Text(
-                                'Phishing attacks are the most common cause of data breaches globally and have'
-                                ' been the root cause of notable instances of cybercrime in the last decade.'),
-                            trailing: Icon(
-                              _customTileExpanded
-                                  ? Icons.arrow_drop_down_circle
-                                  : Icons.arrow_drop_down,
-                            ),
+                        subtitle: const Text(
+                            'Phishing attacks are the most common cause of data breaches globally and have'
+                            ' been the root cause of notable instances of cybercrime in the last decade.'),
+                        trailing: Icon(
+                          _customTileExpanded
+                              ? Icons.arrow_drop_down_circle
+                              : Icons.arrow_drop_down,
+                        ),
+                        children: [
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
                             children: [
-                              Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Text(
-                                      'Phishing attacks attempt to steal information from users or trick them into downloading malware by'
-                                      'sending malicious emails or text messages (SMS) that look like real requests but are, in fact, a Scam.\n'
-                                      '\nExample:'),
-                                  SizedBox(
-                                    height: 20,
-                                  ),
-                                  Image.asset('images/amazon.png'),
-                                  SizedBox(
-                                    height: 20,
-                                  ),
-                                  Text(
-                                      'It occurs when an attacker, masquerading as a trusted entity, dupes a victim into opening '
-                                      'an email, instant message, or text message. The recipient is then tricked into clicking a malicious link, '
-                                      'which can lead to the installation of malware, the freezing of the system as part of a ransomware attack '
-                                      'or the revealing of sensitive information.'
-                                      'An attack can have devastating results. For individuals, this includes unauthorized purchases, the stealing of funds, or identify theft.')
-                                ],
-                              )
-                            ])),
-                  )
-                ],
-              ),
-            )));
+                              Text(
+                                  'Phishing attacks attempt to steal information from users or trick them into downloading malware by'
+                                  'sending malicious emails or text messages (SMS) that look like real requests but are, in fact, a Scam.\n'
+                                  '\nExample:'),
+                              SizedBox(
+                                height: 20,
+                              ),
+                              Image.asset('images/amazon.png'),
+                              SizedBox(
+                                height: 20,
+                              ),
+                              Text(
+                                  'It occurs when an attacker, masquerading as a trusted entity, dupes a victim into opening '
+                                  'an email, instant message, or text message. The recipient is then tricked into clicking a malicious link, '
+                                  'which can lead to the installation of malware, the freezing of the system as part of a ransomware attack '
+                                  'or the revealing of sensitive information.'
+                                  'An attack can have devastating results. For individuals, this includes unauthorized purchases, the stealing of funds, or identify theft.')
+                            ],
+                          )
+                        ])),
+              )
+            ],
+          ),
+        )));
   }
 }
